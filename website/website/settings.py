18c97812d9202ebc8711a6531e2a968438e743b5
ebonnecab <edotclaire@gmail.com>
2020-03-06 08:17:31 -0800

    fixed error

--- website/website/settings.py
+++ website/website/settings.py
@@ -126,5 +126,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '/records/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
